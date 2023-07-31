189cf8d3a9cfc87c453a61b8e568e11ef02e1c31
polobryn <polobryn@yahoo.co.uk>
2023-07-31 17:05:36 +0100

    add DB_ENVIRONMENT for production

--- knexfile.js
+++ knexfile.js
@@ -17,7 +17,18 @@ module.exports = {
       }
     }
   },
-
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations:{
+      tableName:'knex_migrations',
+      directory: './migrations',
+    },
+  }
   // staging: {
   //   client: 'postgresql',
   //   connection: {
@@ -34,20 +45,20 @@ module.exports = {
   //   }
   // },
 
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
+  // production: {
+  //   client: 'postgresql',
+  //   connection: {
+  //     database: 'my_db',
+  //     user:     'username',
+  //     password: 'password'
+  //   },
+  //   pool: {
+  //     min: 2,
+  //     max: 10
+  //   },
+  //   migrations: {
+  //     tableName: 'knex_migrations'
+  //   }
+  // }
 
 };
